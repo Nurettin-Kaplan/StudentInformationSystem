4dd3f97259bd7e30087461b678555364cec97763
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-07-12 15:45:13 +0300

    Functions added and the choice data type changed to char.

--- SIS.c
+++ SIS.c
@@ -1,33 +1,73 @@
 #include<stdio.h>
 #include<windows.h>
+#include<string.h>
 #define RED 12		// color definitions
 #define GREEN 10
 #define YELLOW 14
 #define WHITE 15
 
 void Menu();
+void Add();
+void Update();
+void Delete();
+void ViewAll();
+void Search();
 
 int main(void){
 	
-	int choice;
+	char choice;
 	
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, 14);
 	
 	printf("\t\tWelcome To Student Information System\n\n");
 	
-	Menu(&choice);
+	do{
+		Menu(&choice);
+		system("cls");
+		switch(choice){
+			case '1': Add();			break;
+			case '2': Update();			break;
+			case '3': Delete();			break;
+			case '4': ViewAll();		break;
+			case '5': Search();			break;
+			case '6': continue;			break;
+			default: printf("You have entered an invalid option. Please try again.\n"); break;
+		}
+		
+	}while(choice != '6');
 	
 	system("pause");
 	return 0;
 }
 
-void Menu(int* choice){
-	printf("1- Add student\n"
-	"2- Update student information\n"
-	"3- Delete student\n"
-	"4- View all students\n"
-	"5- Search student by number\n"
-	"Select the operation you want to perform: ");
-	scanf("%d", choice);
+void Menu(char* choice){
+	printf(	"1- Add student\n"
+			"2- Update student information\n"
+			"3- Delete student\n"
+			"4- View all students\n"
+			"5- Search student by number\n"
+			"6- Exit the program\n"
+			"Select the operation you want to perform: ");
+	scanf("%s", choice);
+}
+
+void Add(){
+	//
+}
+
+void Update(){
+	//
+}
+
+void Delete(){
+	//
+}
+
+void ViewAll(){
+	//
+}
+
+void Search(){
+	//
 }
