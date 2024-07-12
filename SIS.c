12c2ab540cff0999c5c46cb564e1581b724412e9
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-07-12 16:51:30 +0300

    The function named 'Add' has been completed.

--- SIS.c
+++ SIS.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 #include<windows.h>
 #include<string.h>
+#include<stdlib.h>
 #define RED 12		// color definitions
 #define GREEN 10
 #define YELLOW 14
 #define WHITE 15
+
+struct Student{
+	int num;
+	char name[20];
+	char surname[20];
+	int age;
+	char department[20];
+	float gpa;
+};
 
 void Menu();
 void Add();
@@ -16,9 +26,6 @@ void Search();
 int main(void){
 	
 	char choice;
-	
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 14);
 	
 	printf("\t\tWelcome To Student Information System\n\n");
 	
@@ -42,6 +49,8 @@ int main(void){
 }
 
 void Menu(char* choice){
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 14);
 	printf(	"1- Add student\n"
 			"2- Update student information\n"
 			"3- Delete student\n"
@@ -53,7 +62,32 @@ void Menu(char* choice){
 }
 
 void Add(){
-	//
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 15);
+	
+	struct Student newStudent;
+	
+	printf("Enter the student number: ");
+	scanf("%d", &newStudent.num);
+	printf("Enter the student name: ");
+	scanf("%s", &newStudent.name);
+	printf("Enter the student surname: ");
+	scanf("%s", &newStudent.surname);
+	printf("Enter the student age: ");
+	scanf("%d", &newStudent.age);
+	printf("Enter the student department: ");
+	scanf("%s", &newStudent.department);
+	
+	FILE *file = fopen("student.txt", "a+");
+	if(file == NULL){
+		perror("There was an error opening the file.\n");
+		exit(1);
+	}
+	fprintf(file, "%d %s %s %d %s\n", newStudent.num, &newStudent.name, &newStudent.surname, newStudent.age, &newStudent.department);
+	fclose(file);
+	
+	SetConsoleTextAttribute(hConsole, 10);
+	printf("\n\t\tThe student was successfully added.\n\n");
 }
 
 void Update(){
